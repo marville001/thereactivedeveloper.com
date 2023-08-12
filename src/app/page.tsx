ed7c508cd2c98a04e28b2ec7bc825120bcacd134
Martin Mwangi <mwangimartin1904@gmail.com>
2023-08-12 03:19:53 +0300

    image updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h4 className="text-6xl font-black uppercase ">Marville001</h4>
+    <main className="min-h-screen">
+
+      <div className="min-h-[400px] flex items-center justify-center ">
+        <h4 className="text-6xl font-black uppercase ">Marville001</h4>
+      </div>
+      <div className="min-h-[400px] flex items-center justify-center ">
+        <h4 className="text-6xl font-black uppercase ">Marville001</h4>
+      </div>
+      <div className="min-h-[400px] flex items-center justify-center ">
+        <h4 className="text-6xl font-black uppercase ">Marville001</h4>
+      </div>
+      <div className="min-h-[400px] flex items-center justify-center ">
+        <h4 className="text-6xl font-black uppercase ">Marville001</h4>
+      </div>
     </main>
   );
 }
