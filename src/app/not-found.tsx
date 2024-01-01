84dde14153d9255585860c75d1d1995ed2135709
Martin Mwangi <mwangimartin1904@gmail.com>
2024-01-01 23:06:49 +0300

    update not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,8 +8,8 @@ const NotFound = () => {
 			<p className="max-w-[500px] text-[20px] text-center">We couldn&apos;t locate that page, sorry about that! Please check the URL and try again, or you can head back to our
 			</p>
 			<div className="flex gap-4 mt-8">
-				<Link href="/" className="text-primary ml-2 hover:bg-accent/70 transition capitalize bg-accent py-1.5 px-8 rounded-md">Home</Link>
-				<Link href="/contact" className="text-primary hover:bg-accent transition hover:text-white ml-2 capitalize border py-1.5 px-8 rounded-md text-accent border-accent">Contact Me</Link>
+				<Link href="/" className="text-white ml-2 hover:bg-accent/70 transition capitalize bg-accent py-1.5 px-8 rounded-md">Home</Link>
+				<Link href="/contact" className="hover:bg-accent transition hover:text-white ml-2 capitalize border py-1.5 px-8 rounded-md text-accent border-accent">Contact Me</Link>
 			</div>
 		</section>
 	);
