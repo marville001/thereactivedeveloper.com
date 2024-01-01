475178c3ab8f64b25efc07f8b255abede5d3d1af
Martin Mwangi <mwangimartin1904@gmail.com>
2024-01-01 21:29:31 +0300

    home page updates

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,153 @@
+import { Github, Link2, Linkedin, MapPin } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import HeroImage from "./hero-image";
+
+export default function Home() {
+  return (
+    <section>
+
+      <div className="section grid grid-cols-1 py-10 md:py-16 lg:py-20 md:max-h-[600px] md:grid-cols-2 max-w-screen-lg mx-auto">
+
+        <div className="px-4 py-8 sm:px-10">
+          <h2 className="flex items-center gap-4 font-bold text-dark dark:text-white opacity-30">
+            <MapPin />
+            <span>Nyeri, Kenya</span>
+          </h2>
+
+          <h1 className="mt-10 text-3xl font-bold text-accent sm:text-4xl">Martin Mwangi</h1>
+          <h1 className="mt-5 mb-6 text-xl font-bold opacity-75 text-dark dark:text-white sm:text-2xl">
+            Full Stack Developer
+          </h1>
+          <p className="mt-5 max-w-[700px] text-dark dark:text-white">
+            With some years of experience in software development, I am dedicated
+            to designing and developing User Interfaces and experiences focused on
+            their emotions ( UI / UX ), used in web platforms and apps.
+          </p>
+          <div className="flex gap-5 my-8">
+            <Link
+              href="https://www.linkedin.com/in/marville001/"
+              target="_blank"
+              className="flex items-center px-2 py-1 text-white rounded-md cursor-pointer bg-primary"
+            >
+              <Linkedin className="pr-2 text-2xl" />
+              <div className="h-4 w-[1px] bg-gray-300"></div>
+              <span className="pl-2 text-lg">LinkedIn</span>
+
+            </Link>
+            <Link
+              href="https://github.com/marville001"
+              target="_blank"
+              className="flex items-center px-2 py-1 text-white rounded-md cursor-pointer bg-primary"
+            >
+              <Github className="pr-2 text-2xl" />
+              <div className="h-4 w-[1px] bg-gray-300"></div>
+              <span className="pl-2 text-lg">Github</span>
+            </Link>
+          </div>
+        </div>
+        <div
+          className="flex items-center justify-center overflow-hidden h-[400px]"
+        >
+          <HeroImage />
+        </div>
+      </div>
+
+      <div className="bg-gray-100 section dark:bg-dark dark:text-white">
+        <div className="max-w-screen-lg mx-auto grid grid-cols-1 md:py-20 md:grid-cols-2">
+          <div className="z-20 px-2 py-10 sm:px-10">
+            <h2 className="mb-6 text-3xl z-[2] capitalize font-bold text-dark dark:text-white">
+              Experience and knowledge to highlight
+            </h2>
+            <div className="">
+              <p className="my-2">
+                <strong className="mr-2 text-lg font-bold text-dim-dark dark:text-white">
+                  +3 years
+                </strong>
+                <span className="opacity-60">
+                  Designing and Developing web applications using ReactJs, NextJs and NodeJs (MERN Stack), etc
+                </span>
+              </p>
+              <p className="my-2">
+                <strong className="mr-2 text-lg font-bold text-dim-dark dark:text-white">
+                  +1 years
+                </strong>
+                <span className="opacity-60">
+                  Designing and Developing mobile applications using React Native
+                </span>
+              </p>
+              <p className="my-2">
+                <strong className="mr-2 text-lg font-bold text-dim-dark dark:text-white">
+                  +3 years
+                </strong>
+                <span className="opacity-60">Using MERN Stack</span>
+              </p>
+
+              <div className="my-4">
+                <p className="">
+                  Currently working with ReactJs, NodeJs, GraphQl, TypeScript and
+                  PostgresDB
+                </p>
+                <Link href="/now"  className='inline-block px-4 py-1 mt-2 text-sm text-white rounded-md bg-primary'>View Current </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2 sm:px-6 md:px-6">
+            <div className="z-[200] -translate-y-0  rounded-t-md bg-[#ffffff] py-4 px-2 dark:bg-dim-dark sm:px-6">
+              <Link
+                href="https://api.whatsapp.com/send?phone=254700207054&text=Hey Martin"
+                target="_blank"
+                className="flex animate-pulse hover:animate-none items-center justify-between rounded-md bg-[#358a76] px-5 py-3 text-lg text-white lg:text-xl"
+              >
+                <svg fill="#000000" className="text-lg lg:text-2xl h-8 text-white w-8" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 308 308" xmlSpace="preserve">
+                  <g id="XMLID_468_">
+                    <path id="XMLID_469_" fill="currentColor" d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
+		c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687
+		c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887
+		c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153
+		c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348
+		c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802
+		c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922
+		c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0
+		c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458
+		C233.168,179.508,230.845,178.393,227.904,176.981z"/>
+                    <path id="XMLID_470_" fill="currentColor" d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
+		c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396
+		c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z
+		 M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188
+		l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677
+		c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867
+		C276.546,215.678,222.799,268.994,156.734,268.994z"/>
+                  </g>
+                </svg>
+                <span>+254700207054</span>
+                <Link2 className="text-lg opacity-50 lg:text-2xl" />
+              </Link>
+
+              <p className="my-4 text-sm tracking-wide text-black opacity-70">
+                Contact me by WhatsApp, you will establish communication with me
+                directly. You will be able to ask or make arrangement for further
+                communications about any task you have.
+              </p>
+
+              <div className="w-full p-6 mt-16 rounded-lg _shadow dark:bg-dark">
+                <h2 className="mb-2 text-lg font-bold">Schedule a meeting</h2>
+                <p className="text-sm font-medium opacity-70">
+                  Choose the ideal moment and meet with the team to put your ideas
+                  into action.
+                </p>
+
+                <Link target="_blank" href="https://cal.com/marville001/30min" className="w-full py-2 px-5 block mt-6 text-sm text-center text-white border-0 rounded-full lg:text-md btn hover:bg-primary bg-opacity-80 hover:bg-opacity-100 bg-primary">
+                  Click to coordinate meeting
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#3386B1",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
