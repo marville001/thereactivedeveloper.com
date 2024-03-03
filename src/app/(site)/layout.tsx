ec6e345c653b11c15a966e5702da22c248e93849
Martin Mwangi <mwangimartin1904@gmail.com>
2024-03-03 09:36:32 +0300

    metadata update for root pages

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,41 +1,9 @@
 import { Footer } from "@/components/shared/footer";
 import Header from "@/components/shared/header";
+import { METADATAS } from "@/constants/metadata";
 import type { Metadata } from 'next';
 
-const title = "Martin Mwangi (Marville001) - Software Developer: React, Next.js, Tailwind CSS";
-
-const description = "Discover the digital prowess of Martin Mwangi, a dedicated Frontend Developer. Through the dynamic synergy of React, Next.js, and Tailwind CSS, Martin crafts flawless web experiences that captivate and engage. With a keen focus on user-centric design and backed by Express.js and Node.js expertise, Martin Mwangi brings your digital aspirations to life. Elevate your online presence with web solutions that reflect elegance, speed, and functionality, all curated by the mastery of Martin Mwangi.";
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://thereactivedeveloper.com'),
-  title,
-  description,
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
-  openGraph: {
-    title,
-    description,
-    images: [
-      "/me.jpg"
-    ],
-  },
-  twitter: {
-    title,
-    description,
-    images: [
-      "/me.jpg"
-    ],
-    card: "summary_large_image",
-  },
-  alternates: {
-    canonical: `/`,
-    languages: {
-      'en-US': '/en-US'
-    },
-  },
-};
+export const metadata: Metadata = METADATAS.home;
 
 export default function RootLayout({
   children,
