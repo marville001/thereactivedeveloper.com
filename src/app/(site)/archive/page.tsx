8aa543bbce6d6d8ec08f9a3f3c55d7c04b80b3b5
Martin Mwangi <mwangimartin1904@gmail.com>
2024-03-24 20:34:39 +0300

    archive coming soon

---
+++ src/app/(site)/archive/page.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { METADATAS } from "@/constants/metadata";
+
+export const metadata: Metadata = METADATAS.archive;
+
+const ArchivedProjects = () => {
+
+	return (
+		<section className="lg:py-4">
+			<div className="md:px-6 dark:text-white mx-auto min-h-[60vh] max-w-[900px] py-10 md:py-24 px-2 sm:px-4">
+				<div className='flex justify-center'>
+					<h1 className="text-3xl lg:text-5xl font-bold text-center space-y-2">
+						Archive
+					</h1>
+				</div>
+
+				<div className="mt-5 flex justify-center flex-wrap">
+					<h1 className='text-center max-w-xl text-3xl mt-24 uppercase font-bold opacity-50'>
+						COMING SOON
+					</h1>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default ArchivedProjects;
