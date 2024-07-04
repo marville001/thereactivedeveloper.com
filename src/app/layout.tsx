37b27eea09908c0e8039ec91fe1c847348d78bc4
Martin Mwangi <mwangimartin1904@gmail.com>
2024-07-04 10:35:01 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+{/*       <head>
       <script id="chatflot" dangerouslySetInnerHTML={{
 					__html: `
      window.chatbotConfig = {
@@ -20,15 +20,15 @@ chatbotId: 'cly6s9ink00018msf58ekx645',
      `}}>
       
       </script>
-      </head>
+      </head> */}
       <body className={cn("dark", openSans.className)}>
         {children}
 
-        <script async src="https://app.chatflot.com/chatwindow.js"></script>
+{/*         <script async src="https://app.chatflot.com/chatwindow.js"></script> */}
 
-        <div id="chatflot-chatbot"> </div>
+{/*         <div id="chatflot-chatbot"> </div> */}
 
-        <script async src="https://app.chatflot.com/chatbot.js"></script>
+{/*         <script async src="https://app.chatflot.com/chatbot.js"></script> */}
       </body>
     </html>
   );
