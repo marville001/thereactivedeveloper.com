9d4fdaa47b1a790a3f882a8ec47f43e93ea159fc
Martin Mwangi <mwangimartin1904@gmail.com>
2024-07-04 08:33:24 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <script>
+      window.chatbotConfig = {
+chatbotId: 'cly6s9ink00018msf58ekx645',
+}
+      </script>
+      </head>
       <body className={cn("dark", openSans.className)}>
         {children}
+
+        <script src="https://app.chatflot.com/chatwindow.js"></script>
+
+        <div id="chatflot-chatbot"> </div>
+
+        <script src="https://app.chatflot.com/chatbot.js"></script>
       </body>
     </html>
   );
